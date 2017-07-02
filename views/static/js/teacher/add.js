3f02bc342fa82709f8e4bd23aae7e2f96f56cb0f
zhaomingyj <936810496@qq.com>
2017-07-02 21:30:46 +0800

    讲师添加和讲师编辑表单验证功能完善，引入插件

--- views/static/js/teacher/add.js
+++ views/static/js/teacher/add.js
@@ -1,4 +1,4 @@
-define(["jquery", "template","util","form","bootstrap-datepicker","bootstrap-datepicker-zh"],function($,template,util){
+define(["jquery", "template", "util", "form", "datepicker", "datepicker-zh", "validate"],function($,template,util){
     // 讲师列表中可以为编辑讲师地址栏中传入自定义属性id,
     // 这样编辑讲师中有id,添加讲师列表地址栏中没有参数，通过地址栏参数区分
     var query = util.getQueryObj();
@@ -16,10 +16,55 @@ define(["jquery", "template","util","form","bootstrap-datepicker","bootstrap-dat
                 data.result.type = "edit";
                 var html = template("teacheraddTpl",data.result);
                 $(".teacher").html(html);
+                // 添加日期插件
                 $("input[name=tc_join_date]").datepicker({
                     format:"yyyy-mm-dd",
                     language:"zh-CN"
-                })
+                });
+                // 添加表单验证
+                $("#teacherform").validate({
+                    description:{
+                        "tcname": {
+                            required:"请输入用户名",
+                        },
+                        "tcpass":{
+                            required:"请输入密码",
+                        },
+                        "tcjoindate":{
+                            required:"请输入入职时间",
+                        }
+                    },
+                    onBlur:true,
+                    onKeyup:true,
+                    sendForm:false,
+                    eachInvalidField:function(){
+                        this.parent().parent().addClass("has-error").removeClass("has-success");
+                        this.parent().next().removeClass("hide");    
+                    },
+                    eachValidField:function(){
+                        this.parent().parent().addClass("has-success").removeClass("has-error");
+                    },
+                    valid:function(){
+                        console.log("valid 表单验证通过");
+                        var type = $("#btnSave").data("type");
+                        var url = "";
+                        if(type == "edit"){
+                            url="/api/teacher/update";            
+                        }else{
+                            url="/api/teacher/add";            
+                        }
+                        $("#teacherform").ajaxSubmit({
+                            url:url,
+                            type:"post",
+                            success:function(data){
+                                if(data.code == 200){
+                                    console.log(data);
+                                    location.href = "/teacher/list";                    
+                                }
+                            }
+                        });                
+                    }
+                });                
             }
         });
     }else{ //添加讲师列     
@@ -29,30 +74,54 @@ define(["jquery", "template","util","form","bootstrap-datepicker","bootstrap-dat
             type : "add"
         });
         $(".teacher").html(html);
+        // 添加日期插件
         $("input[name=tc_join_date]").datepicker({
-            format:"yyyy-mm-dd",
-            language:"zh-CN"
-        })
-    }
-    // 点击保存按钮注册点击事件
-    $(".teacher").on("click","#btnSave",function(){      
-        var type = $(this).data("type");
-        var url = "";
-        if(type == "edit"){
-            url="/api/teacher/update";            
-        }else{
-            url="/api/teacher/add";            
-        }
-        $("#teacherform").ajaxSubmit({
-            url:url,
-            type:"post",
-            success:function(data){
-                if(data.code == 200){
-                    console.log(data);
-                    location.href = "/teacher/list";                    
+            format: "yyyy-mm-dd",
+            language: "zh-CN"
+		});
+        // 添加表单验证
+        $("#teacherform").validate({
+            description:{
+                "tcname": {
+                    required:"请输入用户名",
+                },
+                "tcpass":{
+                    required:"请输入密码",
+                },
+                "tcjoindate":{
+                    required:"请输入入职时间",
                 }
+            },
+            onBlur:true,
+            onKeyup:true,
+            sendForm:false,
+            eachInvalidField:function(){
+                this.parent().parent().addClass("has-error").removeClass("has-success");
+                this.parent().next().removeClass("hide");    
+            },
+            eachValidField:function(){
+                this.parent().parent().addClass("has-success").removeClass("has-error");
+            },
+            valid:function(){
+                console.log("valid 表单验证通过");
+                var type = $("#btnSave").data("type");
+                var url = "";
+                if(type == "edit"){
+                    url="/api/teacher/update";            
+                }else{
+                    url="/api/teacher/add";            
+                }
+                $("#teacherform").ajaxSubmit({
+                    url:url,
+                    type:"post",
+                    success:function(data){
+                        if(data.code == 200){
+                            console.log(data);
+                            location.href = "/teacher/list";                    
+                        }
+                    }
+                });                
             }
-        });
-        return false;
-    })
+        })
+    } 
 })
--- views/static/libs/config.js
+++ views/static/libs/config.js
@@ -1,22 +1,31 @@
 require.config({
-    baseUrl:"/views",
-    paths:{
-        "jquery":"assets/jquery/jquery.min",
-        "form":  "assets/jquery-form/jquery.form",
-        "cookie":"assets/jquery-cookie/jquery.cookie",
-        "template":"assets/artTemplate/template",
-        "bootstrap":"assets/bootstrap/js/bootstrap.min",
-        "util":"static/js/util",
-        "nprogress":"assets/nprogress/nprogress",
-        "bootstrap-datepicker":"assets/bootstrap-datepicker/js/bootstrap-datepicker.min",
-        "bootstrap-datepicker-zh":"assets/bootstrap-datepicker/locales/bootstrap-datepicker.zh-CN.min"     
-    },
-    shim:{
-        "bootstrap":{
-            deps:["jquery"]
-        },
-        "bootstrap-datepicker-zh":{
-            deps:["bootstrap-datepicker"]
-        }
-    }
+     baseUrl: "/views",
+     paths: {
+     	"jquery": "assets/jquery/jquery.min",
+     	"form": "assets/jquery-form/jquery.form",
+     	"cookie": "assets/jquery-cookie/jquery.cookie",
+     	"template": "assets/artTemplate/template",
+     	"bootstrap": "assets/bootstrap/js/bootstrap.min",
+     	"util": "static/js/util",
+          "nprogress": "assets/nprogress/nprogress",
+          "datepicker": "assets/bootstrap-datepicker/js/bootstrap-datepicker.min",
+          "datepicker-zh": "assets/bootstrap-datepicker/locales/bootstrap-datepicker.zh-CN.min",
+          "validate": "assets/jquery-validate/jquery-validate.min",
+          "ckeditor": "assets/ckeditor/ckeditor",
+          "region": "assets/jquery-region/jquery.region"
+     },
+     shim: {
+     	"bootstrap": {
+     		deps: ["jquery"]
+     	},
+          "datepicker-zh": {
+               deps: ["datepicker"]
+          },
+          "validate": {
+               deps: ["jquery"]
+          },
+          "ckeditor": {
+               exports: "CKEDITOR"
+          }
+     }
 });
